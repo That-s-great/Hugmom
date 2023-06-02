3dd6e0bd9ae5f92dee497514518c3839022bf040
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2023-06-03 01:36:15 +0900

    itemdetail

--- js/mall_itemdetail.js
+++ js/mall_itemdetail.js
@@ -1,17 +1,41 @@
 const minus = document.querySelector(".fa-minus");
 const plus = document.querySelector(".fa-plus");
 const price = document.querySelector(".price");
+const num = document.querySelector(".num");
+const total = document.querySelector(".total");
+const comment = document.querySelectorAll(".comment");
+
+const pricecal = price.innerText.split(",").join("");
 let i = 1;
 minus.addEventListener("click", () => {
-  const num = document.querySelector(".num");
-  i--;
-  num.innerText = i;
+  if (i > 1) {
+    i--;
+    num.innerText = i;
+    total.innerText = (parseInt(pricecal) * i).toLocaleString();
+  } else {
+    minus.setAttribute("disabled", "disabled");
+  }
 });
 plus.addEventListener("click", () => {
-  const num = document.querySelector(".num");
-  const total = document.querySelector(".total");
-
   i++;
   num.innerText = i;
-  total.innerText = parseInt(price.innerText) * i;
+  total.innerText = (parseInt(pricecal) * i).toLocaleString();
 });
+
+// comment.forEach(function (e) {
+//   e.addEventListener("click", toggleAccordion);
+// });
+
+// function toggleAccordion(e) {
+//   let comment_content = e.currentTarget.classList;
+//   console.log(comment_content);
+
+//   if (comment_content.contains("open")) {
+//     comment_content.remove("open");
+//   } else {
+//     comment.forEach(function (e) {
+//       e.classList.remove("open");
+//     });
+//     comment_content.add("open");
+//   }
+// }

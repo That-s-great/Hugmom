4551911060a9104ad9dacdb2e124a0f3b0a213b2
Dami-LEE00 <58524208+Dami-LEE00@users.noreply.github.com>
2023-06-02 11:46:36 +0900

    Delete js directory

--- js/08-mypage.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.mypage_user_info li a');
-
-function menuClick() {
-  if(menu.classList.contains('mypage_user_info_select')) {
-    menu.classList.remove('mypage_user_info_select');
-  } else {
-    menu.classList.add('mypage_user_info_select');
-  }
-}

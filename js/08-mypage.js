d08806d7ac61da96e0b7e9c1d152d6a7cd21bdda
Dami-LEE00 <ecch74@naver.com>
2023-06-13 18:19:30 +0900

    update 08-mypage.js

--- js/08-mypage.js
+++ js/08-mypage.js
@@ -1,29 +1,75 @@
-// 08-2-edit(2)
-const inputPw = document.querySelector('.mypage_userInfo_identification .inputPw').value;
+// 08-2-mypage-edit(1)
+const inputPw = document.querySelector('.mypage_userInfo_identification .inputPw');
 const inputCode = document.querySelector('.mypage_userInfo_identification .inputCode');
 const errorPw = document.querySelector('.mypage_userInfo_identification .errorM_pw');
 const errorCode = document.querySelector('.mypage_userInfo_identification .errorM_code');
-const indentificationBtn = document.querySelector('.mypage_userInfo_identification .indentification_btn');
+const identificationBtn = document.querySelector('.mypage_userInfo_identification .identification_btn');
 
-// function showError() {
-//   if(inputPw.value !== "") {
-//     if(inputCode.value !== "") {
-//       window.location.href = "/html/08-2-mypage-edit(2)";
-//     } else {
-//       alert("입력란이 비어있습니다.");
-//       errorCode.style.display = "inline-block";
-//     }
-//   } else {
-//     alert("입력란이 비어있습니다.");
-//     errorPw.style.display = "inline-block";
-//     errorCode.style.display = "inline-block";
-//   }
-// }
-
-function showError() {
-  if(inputPw == "") {
-    errorPw.add.classList = "errorM_pw";
+inputPw.addEventListener('input', () => {
+  if(inputPw.value === '') {
+    errorPw.style.display = 'inline-block';
   } else {
-    errorPw.classList.remove() = "errorM_pw";
+    errorPw.style.display = 'none';
   }
-}
+});
+
+inputCode.addEventListener('input', () => {
+  if(inputCode.value === '') {
+    errorCode.style.display = 'inline-block';
+  } else {
+    errorCode.style.display = 'none';
+  }
+});
+
+identificationBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if(inputPw.value !== '' && inputCode.value !== '') {
+    alert('본인인증이 완료되었습니다.');
+    window.location.href = '08-2-mypage-edit(2).html';
+  } else {
+    alert('입력란이 비어있습니다.');
+    errorPw.style.display = 'inline-block';
+    errorCode.style.display = 'inline-block';
+  }
+  inputPw.value = '';
+  inputCode.value = '';
+});
+
+
+
+
+// 08-2-mypage-edit(2)
+const resetPw = document.querySelector('.mypage_userInfo_edit .resetPw');
+const resetPwChk = document.querySelector('.mypage_userInfo_edit .resetPw_chk');
+const errorResetPw = document.querySelector('.mypage_userInfo_edit .errorPw');
+
+const birth = document.querySelectorAll('.mypage_userInfo_edit .birth');
+const errorBirth = document.querySelector('.mypage_userInfo_edit .errorBirth');
+
+const email = document.querySelectorAll('.mypage_userInfo_edit .email');
+const errorEmail = document.querySelector('.mypage_userInfo_edit .errorEmail');
+
+const addressHome = document.querySelectorAll('.mypage_userInfo_edit .addressHome');
+const errorAddHome = document.querySelector('.mypage_userInfo_edit .errorAddHome');
+
+const addressCenter = document.querySelectorAll('.mypage_userInfo_edit .addressCenter');
+const errorAddCenter = document.querySelector('.mypage_userInfo_edit .errorCenter');
+
+const p1 = document.querySelector('.mypage_userInfo_edit #p1');
+const p2 = document.querySelector('.mypage_userInfo_edit #p2');
+const p3 = document.querySelector('.mypage_userInfo_edit #p3');
+
+const editBtn = document.querySelector('.edit_btn.editBtn');
+
+editBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if(resetPw.value !== '' && resetPwChk.value !== '' && birth !== '' && birth !== '선택' && email.value !== '' 
+  && addressHome.value !== '' && addressCenter.value !== '' && p1 !== '' && p2 !== '' && p3 !== '') {
+    alert('회원정보가 수정되었습니다.');
+    window.location.href = '08-1-mypage.html';
+  } else {
+    alert('입력란이 비어있습니다.');
+  }
+});

5660a3ce2571923105aa6c766b87febf9910c32b
Dami-LEE00 <ecch74@naver.com>
2023-06-19 18:19:05 +0900

    update 08 mypage js

--- js/08-3-mypage-order.js
+++ js/08-3-mypage-order.js
@@ -1,30 +1,154 @@
 // 08-3-mypage-order
-// 주문종류 & 조회기간 버튼
-const ordertypeSpans = document.querySelectorAll('.order_type .btn span');
-const periodSpans = document.querySelectorAll('.period span');
-// 조회 버튼
+const allProduct = document.querySelector('.order_type .allProduct');
+const reservedProduct = document.querySelector('.order_type .reservedProduct');
+
+const month1 = document.querySelector('.inquiry_period .month1');
+const month3 = document.querySelector('.inquiry_period .month3');
+const month6 = document.querySelector('.inquiry_period .month6');
+const year1 = document.querySelector('.inquiry_period .year1');
+
 const orderChkBtn = document.querySelector('.orderChk_btn');
-// 조회 시 뜨는 주문내역 리스트
-const historyBox = document.querySelector('.mypage_shoppingInfo_order_history');
+const historyBox = document.querySelectorAll('.mypage_shoppingInfo_order_history .contents');
+const history1 = document.querySelector('.mypage_shoppingInfo_order_history .history1');
+const history2 = document.querySelector('.mypage_shoppingInfo_order_history .history2');
+const history3 = document.querySelector('.mypage_shoppingInfo_order_history .history3');
+const history4 = document.querySelector('.mypage_shoppingInfo_order_history .history4');
+const reserve1 = document.querySelector('.mypage_shoppingInfo_order_history .reserve1');
 
 
-ordertypeSpans.forEach(span => {
-  span.addEventListener('click', () => {
-    ordertypeSpans.forEach(span => {
-      span.classList.remove('selected_btn');
-    });
-    span.classList.add('selected_btn');
-  });
+// 조회 버튼 눌렀을 시 주문내역 표시
+orderChkBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (reservedProduct.classList.contains('selected_btn')) {
+    // reservedProduct를 눌렀을 때
+    history1.style.display = 'none';
+    history2.style.display = 'none';
+    history3.style.display = 'none';
+    history4.style.display = 'none';
+    reserve1.style.display = 'flex';
+  } else {
+    // reservedProduct 이외의 경우
+    if (allProduct.classList.contains('selected_btn')) {
+      if (month1.classList.contains('selected_btn')) {
+        history1.style.display = 'flex';
+        history2.style.display = 'none';
+        history3.style.display = 'none';
+        history4.style.display = 'none';
+        reserve1.style.display = 'none';
+      } else if (month3.classList.contains('selected_btn')) {
+        history1.style.display = 'flex';
+        history2.style.display = 'flex';
+        history3.style.display = 'none';
+        history4.style.display = 'none';
+        reserve1.style.display = 'none';
+      } else if (month6.classList.contains('selected_btn')) {
+        history1.style.display = 'flex';
+        history2.style.display = 'flex';
+        history3.style.display = 'flex';
+        history4.style.display = 'none';
+        reserve1.style.display = 'none';
+      } else {
+        history1.style.display = 'flex';
+        history2.style.display = 'flex';
+        history3.style.display = 'flex';
+        history4.style.display = 'flex';
+        reserve1.style.display = 'none';
+      }
+    } else {
+      historyBox.style.display = 'none';
+    }
+  }
 });
 
-periodSpans.forEach(span => {
-  span.addEventListener('click', () => {
-    periodSpans.forEach(span => {
-      span.classList.remove('selected_btn');
-    });
-    span.classList.add('selected_btn');
+
+// 주문종류 선택
+function orderType() {
+  // 주문종류 > 전체
+  allProduct.addEventListener('click', () => {
+    if(!allProduct.classList.contains('selected_btn')) {
+      allProduct.classList.add('selected_btn');
+      reservedProduct.classList.remove('selected_btn');
+    }
   });
-});
+  // 주문종류 > 예약상품
+  reservedProduct.addEventListener('click', () => {
+    if(!reservedProduct.classList.contains('selected_btn')) {
+      reservedProduct.classList.add('selected_btn');
+      allProduct.classList.remove('selected_btn');
+    }      
+  });
+}
+
+// 조회기간 선택
+function inquiryPeriod() {
+  // 조회기간 > 1개월
+  month1.addEventListener('click', () => {
+    if(!month1.classList.contains('selected_btn')) {
+      month1.classList.add('selected_btn');
+      month3.classList.remove('selected_btn');
+      month6.classList.remove('selected_btn');
+      year1.classList.remove('selected_btn');
+    }
+  });
+  // 조회기간 > 3개월
+  month3.addEventListener('click', () => {
+    if(!month3.classList.contains('selected_btn')) {
+      month3.classList.add('selected_btn');
+      month1.classList.remove('selected_btn');
+      month6.classList.remove('selected_btn');
+      year1.classList.remove('selected_btn');
+    }
+  });
+  // 조회기간 > 6개월
+  month6.addEventListener('click', () => {
+    if(!month1.classList.contains('selected_btn')) {
+      month6.classList.add('selected_btn');
+      month1.classList.remove('selected_btn');
+      month3.classList.remove('selected_btn');
+      year1.classList.remove('selected_btn');
+    }
+  });
+  // 조회기간 > 1년
+  year1.addEventListener('click', () => {
+    if(!year1.classList.contains('selected_btn')) {
+      year1.classList.add('selected_btn');
+      month1.classList.remove('selected_btn');
+      month3.classList.remove('selected_btn');
+      month6.classList.remove('selected_btn');
+    }
+  });
+}
+
+orderType();
+inquiryPeriod();
 
 
 
+
+
+
+
+
+// querySelectorAll로 하는 방법
+// const periodSpans = document.querySelectorAll('.period span');
+
+// periodSpans.forEach(span => {
+//   span.addEventListener('click', () => {
+//     periodSpans.forEach(span => {
+//       span.classList.remove('selected_btn');
+//     });
+//     span.classList.add('selected_btn');
+//   });
+// });
+
+// const ordertypeSpans = document.querySelectorAll('.order_type .btn span');
+
+// ordertypeSpans.forEach(span => {
+//   span.addEventListener('click', () => {
+//     ordertypeSpans.forEach(span => {
+//       span.classList.remove('selected_btn');
+//     });
+//     span.classList.add('selected_btn');
+//   });
+// });

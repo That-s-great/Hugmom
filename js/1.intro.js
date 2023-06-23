cf2bf56eec7532e7e75158891739d57e895bd9e1
greenyun2 <cys4875@naver.com>
2023-06-23 21:30:27 +0900

    fileAdd

--- js/1.intro.js
+++ js/1.intro.js
@@ -1,7 +1,5 @@
 // Banner Slides
-const slideWrapper = document.querySelector(
-  ".intro_page_banner_section_slide_wrapper"
-);
+const slideWrapper = document.querySelector(".intro_page_banner_section_slide_wrapper");
 const slides = document.querySelectorAll(".intro_page_banner_section_slide");
 const prevBtn = document.querySelector(".banner_slide_prev_btn");
 const nextBtn = document.querySelector(".banner_slide_next_btn");
@@ -13,27 +11,26 @@ let slideCount = slides.length;
 let slideWidth = slides[0].offsetWidth;
 let slideInterval;
 
-indexLength.innerText = slides.length;
-+1;
+indexLength.innerText = slides.length; + 1
 
 function startSlideInterval() {
   slideInterval = setInterval(() => {
     moveSlides(1);
-  }, 3000);
+  }, 3000)
 }
 
 function stopSlideInterval() {
   clearInterval(slideInterval);
 }
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener('click', () => {
   moveSlides(-1);
-  stopSlideInterval();
+  stopSlideInterval()
 });
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener('click', () => {
   moveSlides(1);
-  stopSlideInterval();
+  stopSlideInterval()
 });
 
 function moveSlides(direction) {
@@ -64,17 +61,16 @@ function handleMouseOut() {
   startSlideInterval();
 }
 
-slideWrapper.addEventListener("mouseover", handleMouseOver);
-slideWrapper.addEventListener("mouseout", handleMouseOut);
+slideWrapper.addEventListener('mouseover', handleMouseOver);
+slideWrapper.addEventListener('mouseout', handleMouseOut);
 
 startSlideInterval();
+
 
 // 스크롤 이벤트를 감지하는 함수
 function scrollHandler() {
   // 보여질 요소를 선택합니다
-  let elementsToShow = document.querySelectorAll(
-    ".intro_page_introduce_article"
-  );
+  let elementsToShow = document.querySelectorAll('.intro_page_introduce_article');
 
   // 각 요소에 대해 확인합니다
   for (let i = 0; i < elementsToShow.length; i++) {
@@ -82,7 +78,7 @@ function scrollHandler() {
     // 요소가 보이는지 확인합니다
     if (isElementInViewport(element)) {
       // 보이는 요소에 클래스를 추가하여 나타나도록 설정합니다
-      element.classList.add("show");
+      element.classList.add('show');
       element.style.opacity = 1; // 불투명도를 1로 변경하여 보이게 설정합니다
     } else {
       element.style.opacity = 0; // 불투명도를 0으로 변경하여 숨깁니다
@@ -96,17 +92,19 @@ function isElementInViewport(el) {
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
+
 // 스크롤 이벤트에 scrollHandler 함수를 연결합니다
-window.addEventListener("scroll", scrollHandler);
+window.addEventListener('scroll', scrollHandler);
 
 // 초기에 한 번 호출하여 현재 보이는 요소를 확인합니다
 scrollHandler();
+
+
 
 // Live Slides
 
@@ -115,19 +113,20 @@ const liveNextBtn = document.querySelector(".live_slide_next_btn");
 const liveSlideWrapper = document.querySelector(".live_broadcast_items");
 const liveSlides = document.querySelectorAll(".live_item");
 
+
 let liveCurrentIdx = 0;
 const liveSlideCount = liveSlides.length;
 const liveSlideWidth = liveSlides[0].offsetWidth;
-const liveSlideMargin = 20;
+const liveSlideMargin = 20
 
-livePrevBtn.addEventListener("click", () => {
+livePrevBtn.addEventListener('click', () => {
   if (liveCurrentIdx > 0) {
     liveCurrentIdx--;
     moveLiveSlide();
   }
 });
 
-liveNextBtn.addEventListener("click", () => {
+liveNextBtn.addEventListener('click', () => {
   if (liveCurrentIdx < liveSlideCount - 1) {
     liveCurrentIdx++;
     moveLiveSlide();
@@ -138,5 +137,7 @@ function moveLiveSlide() {
   const translateX = -liveCurrentIdx * (liveSlideWidth + liveSlideMargin);
   liveSlides.forEach((liveSlide) => {
     liveSlide.style.transform = `translateX(${translateX}px)`;
-  });
+  })
 }
+
+

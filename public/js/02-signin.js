178ae2f6ee881c5126d95f3591068040c573f817
yoojin <yoojin4719@gmail.com>
2023-09-26 18:40:15 +0900

    public

--- public/js/02-signin.js
+++ public/js/02-signin.js
@@ -5,19 +5,21 @@ const loginBtn = document.querySelector(".signin_page_login_btn");
 
 loginBtn.addEventListener("click", () => {
   const loginId = "hugmom123";
-  const loginPw = "hugmom123";
+  // const loginPw = "hugmom123";
 
   if (idInput.value === "" && pwInput.value === "") {
     alert("아이디와 비밀번호를 입력해 주세요!");
-  } else if (idInput.value === loginId && pwInput.value === "") {
+  } else if (idInput.value !== "" && pwInput.value === "") {
     alert("비밀번호를 입력해 주세요!");
-  } else if (idInput.value === "" && pwInput.value === loginPw) {
+  } else if (idInput.value === "" && pwInput.value !== "") {
     alert("아이디를 입력해 주세요!");
-  } else if (idInput.value === loginId && pwInput.value !== loginPw) {
-    alert("비밀번호를 확인해 주세요!");
-  } else if (idInput.value !== loginId && pwInput.value === loginPw) {
-    alert("아이디를 확인해 주세요");
-  } else if (idInput.value !== loginId && pwInput.value !== loginPw) {
+  }
+  // else if (idInput.value === loginId && pwInput.value !== "") {
+  //   alert("비밀번호를 확인해 주세요!");
+  // } else if (idInput.value !== loginId && pwInput.value === loginPw) {
+  //   alert("아이디를 확인해 주세요");
+  // }
+  else if (idInput.value === "" && pwInput.value === "") {
     alert("아이디와 비밀번호를 확인해 주세요!");
   } else {
     alert(`환영합니다 ${loginId}님!`);
